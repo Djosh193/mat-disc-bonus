b86fb090d7b9236c4f2d77428e94a44ad2f2ba17
Lucas Yuri <lucasyuri56@hotmail.com>
2021-12-08 15:28:21 -0300

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+void primo (int x, int L, int aux ){
+  if (x <= 2){
+    if (x == 2){
+      printf ("%d É primo.\n", x);
+      return;
+    }
+    else{
+      printf ("%d Não é primo.\n", x);
+      return;
+    }
+  }
+  else{
+    
+    if (aux == L){
+      printf ("%d É primo.\n", x);
+      return;
+    }
+
+    else{
+      if (x % aux == 0){
+        printf ("%d Não é primo.\n", x);
+        return;
+      }
+      primo (x, L, aux+1);
+    }
+  }
+}
+
+int main (){
+  
+  int x; //Número a ser testado se é primo ou não.
+  int aux = 2; //Auxiliar
+  int L; //Limite da busca
+
+  scanf("%d", &x);
+
+  L = ceil(sqrt(x));
+
+  primo (x, L, aux);
+
+  return 0;
+}
