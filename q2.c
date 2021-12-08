e9e788284efadfb1a511b98c4cd7d87fce39bf27
Djosh193 <89870657+Djosh193@users.noreply.github.com>
2021-12-08 16:13:22 -0300

    questão 2

---
+++ q2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+void primo (int x, int L, int aux ){
+  if (x <= 2){
+    if (x == 2){
+      printf ("%d\n", x);
+      return;
+    }
+    else{
+      return;
+    }
+  }
+  else{
+    
+    if (aux > L){
+      printf ("%d\n", x);
+      return;
+    }
+
+    else{
+      if (x % aux == 0){
+        return;
+      }
+      primo (x, L, aux+1);
+    }
+  }
+}
+
+void lista (int limite, int cont){
+  //Parametros pra função
+  int aux = 2; //Auxiliar
+  int L;
+  L = ceil(sqrt(cont));
+
+  //Teste se é primo ou não.
+  if (cont == limite){
+    primo (cont, L, aux);
+    return;
+  }
+  else{
+    primo (cont, L, aux);
+    lista (limite, cont+1);
+  }
+  return;
+}
+
+int main (){
+  
+  int limite; //Número limite da busca por primos
+  int cont = 0; //Contador
+
+  scanf("%d", &limite);
+
+  lista (limite, cont);
+
+  return 0;
+}
